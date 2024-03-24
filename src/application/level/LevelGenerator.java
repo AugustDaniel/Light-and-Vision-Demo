30339126b7d0be74d54c17e8f86ac8238343080c
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2024-03-24 23:02:09 +0100

    changed getlevelborders to make use of renderable

--- src/application/level/LevelGenerator.java
+++ src/application/level/LevelGenerator.java
@@ -6,7 +6,6 @@ import util.LineSegment;
 import java.awt.*;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -18,7 +17,7 @@ public class LevelGenerator {
         for (int x = 0; x < map.length; x++) {
 
             for (int y = 0; y < map[x].length; y++) {
-                if(map[x][y] == 1) {
+                if (map[x][y] == 1) {
                     lineSegments.addAll(
                             new Renderable(
                                     new Point2D.Double(y * blockWidth, x * blockWidth),
@@ -35,27 +34,12 @@ public class LevelGenerator {
     }
 
     public static List<LineSegment> getLevelBorders(double levelWidth, double levelHeight) {
-        List<LineSegment> lineSegments = new LinkedList<>();
-
-        Collections.addAll(lineSegments,
-                new LineSegment(
-                        new Point2D.Double(0, 0),
-                        new Point2D.Double(levelWidth, 0)
-                ),
-                new LineSegment(
-                        new Point2D.Double(levelWidth, 0),
-                        new Point2D.Double(levelWidth, levelHeight)
-                ),
-                new LineSegment(
-                        new Point2D.Double(levelWidth, levelHeight),
-                        new Point2D.Double(0, levelHeight)
-                ),
-                new LineSegment(
-                        new Point2D.Double(0, levelHeight),
-                        new Point2D.Double(0, 0)
-                )
+        return new LinkedList<>(
+                new Renderable(new Point2D.Double(0, 0),
+                new Rectangle2D.Double(0, 0, levelWidth, levelHeight),
+                Color.PINK,
+                1)
+                .getLineSegments()
         );
-
-        return lineSegments;
     }
 }
