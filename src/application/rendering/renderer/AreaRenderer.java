22c23e2c81fb22243124b49ecd2122ae66bd9331
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2024-03-31 19:41:57 +0200

    changed arearenderer to make use of area fills instead of polygons

--- src/application/rendering/renderer/AreaRenderer.java
+++ src/application/rendering/renderer/AreaRenderer.java
@@ -5,17 +5,29 @@ import util.LineSegment;
 
 
 import java.awt.*;
+import java.awt.geom.Area;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
 import java.util.List;
 
 public class AreaRenderer extends Renderer {
 
+    protected Area combinedPoly;
+    protected Point2D pos;
+
     public AreaRenderer(List<LineSegment> rays) {
         super(rays);
+        this.combinedPoly = new Area();
+        this.pos = new Point2D.Double(0, 0);
     }
 
     @Override
     public void update() {
         this.toDraw.clear();
+
+        if (this.rays.isEmpty()) {
+            return;
+        }
 
         for (int i = 0; i < this.rays.size(); i++) {
             LineSegment ray = this.rays.get(i);
@@ -29,13 +41,22 @@ public class AreaRenderer extends Renderer {
 
             this.toDraw.add(p);
         }
+
+        this.combinedPoly = new Area();
+        this.pos = this.rays.get(0).getStart();
+
+        this.toDraw.forEach(e -> {
+                    Area a = new Area(e);
+                    combinedPoly.add(a);
+                }
+        );
     }
 
     @Override
     public void draw(FXGraphics2D graphics) {
         graphics.setColor(Color.BLUE);
-        for (Shape p : this.toDraw) {
-            graphics.fillPolygon((Polygon) p);
-        }
+        graphics.fill(combinedPoly);
+        graphics.setColor(Color.BLACK);
+        graphics.fill(new Ellipse2D.Double(pos.getX() - 12, pos.getY() - 12, 24, 24));
     }
 }
--- src/application/rendering/renderer/LightRenderer.java
+++ src/application/rendering/renderer/LightRenderer.java
@@ -4,42 +4,17 @@ import org.jfree.fx.FXGraphics2D;
 import util.LineSegment;
 
 import java.awt.*;
-import java.awt.geom.Area;
-import java.awt.geom.Point2D;
 import java.util.List;
 
 public class LightRenderer extends AreaRenderer {
 
-    private Area combinedPoly;
-    private Point2D pos;
-
     public LightRenderer(List<LineSegment> rays) {
         super(rays);
-        this.pos = new Point2D.Double(0,0);
-        this.combinedPoly = new Area();
-    }
-
-    @Override
-    public void update() {
-        super.update();
-
-        if (this.rays.isEmpty()) {
-            return;
-        }
-
-        this.combinedPoly = new Area();
-        this.pos = this.rays.get(0).getStart();
-
-        this.toDraw.forEach(e -> {
-                    Area a = new Area(e);
-                    combinedPoly.add(a);
-                }
-        );
     }
 
     @Override
     public void draw(FXGraphics2D graphics) {
-        RadialGradientPaint light = new RadialGradientPaint(pos, 500, new float[]{0.1f, 0.5f} ,new Color[]{Color.YELLOW, Color.BLACK});
+        RadialGradientPaint light = new RadialGradientPaint(pos, 500, new float[]{0.1f, 0.6f} ,new Color[]{Color.YELLOW, Color.BLACK});
         graphics.setPaint(light);
         graphics.fill(combinedPoly);
         graphics.setPaint(null);
